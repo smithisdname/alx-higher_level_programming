e5b27ffc9b95dbacda46c973bdaf6a00893f962e
folarin folayan <crooksmith6@gmail.com>
2023-03-13 04:08:28 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            char = chr(ord(c) - 32)
-        else:
-            char = c
-        print("{:s}".format(char), end="")
-    print('')
+def to_uper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
 
+
+def uppercase(string):
+    string_new = ""
+    for character in string:
+        string_new += "%c" % to_uper(character)
+    print("{:s}".format(string_new))
